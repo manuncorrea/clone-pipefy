7f80e3156306e89f44a7702debeacec1d86e0fbb
Emanuele <manuncorrea@hotmail.com>
2020-03-09 10:34:10 -0300

    Adicionando o build

---
+++ build/precache-manifest.f5b52b5534b2fc6541ce7ce4e5901f5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d871977a175a30e269688bd1fcde80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a21d5ddd5d8b70181186",
+    "url": "/static/js/2.ee504396.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.ee504396.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a600256b061712b34e61",
+    "url": "/static/js/main.1a36ea5f.chunk.js"
+  },
+  {
+    "revision": "cbc171691cd117619945",
+    "url": "/static/js/runtime-main.79ada458.js"
+  }
+]);
